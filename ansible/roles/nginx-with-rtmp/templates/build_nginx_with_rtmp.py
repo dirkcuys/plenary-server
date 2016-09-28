6a734d81d348ceaf6e7f4f141fed2422d79da77c
Charlie DeTar <cfd@media.mit.edu>
2016-09-28 11:40:17 -0600

    Cron rebuilds of nginx-with-rtmp

--- ansible/roles/nginx-with-rtmp/templates/build_nginx_with_rtmp.py
+++ ansible/roles/nginx-with-rtmp/templates/build_nginx_with_rtmp.py
@@ -3,6 +3,8 @@
 import os
 import subprocess
 
+NGINX_RTMP_GIT_URL = "https://github.com/sergey-dryabzhinsky/nginx-rtmp-module.git"
+
 def check_changed(command, changed_func, changed):
     if isinstance(command, str):
         command = command.split()
@@ -10,16 +12,23 @@ def check_changed(command, changed_func, changed):
     result = changed_func(output)
     return result or changed
 
+def fetch_git_repo(url, branch, dest):
+    pass
+
 def main():
-    # remove distro nginx.
     changed = False
 
     def _apt_changed(stdout):
         return "0 upgraded, 0 newly installed, 0 to remove" not in stdout
 
+    def _git_changed(stdout):
+        pass
+
     changed = check_changed("apt-get autoremove nginx", _apt_changed, changed)
     changed = check_changed("apt-get install dpkg-dev build-essential libav-tools",
             _apt_changed, changed)
+
+
 
     if os.path.exists("/usr/src/nginx-rtmp-module/.git"):
         pass
