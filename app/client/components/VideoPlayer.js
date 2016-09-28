c76fd145d0f43875cb78c29ed48551db0ace0138
Charlie DeTar <cfd@media.mit.edu>
2016-09-27 18:54:03 -0600

    All in with HLS

--- app/client/components/VideoPlayer.js
+++ app/client/components/VideoPlayer.js
@@ -26,7 +26,7 @@ class ImmutableVideo extends React.Component {
   }
 
   hasMSE() {
-    return 'MediaSource' in window && shaka.Player.isBrowserSupported();
+    return 'MediaSource' in window && Hls.isSupported();
   }
 
   componentDidMount() {
@@ -42,15 +42,15 @@ class ImmutableVideo extends React.Component {
 
   loadVideo() {
     let path = this.getVideoPathName();
-    let dashUrl = `https://${this.props.conf.videoServerHostname}/dash/${path}/index.mpd`;
+    let hlsUrl = `https://${this.props.conf.videoServerHostname}/hls/${path}/index.m3u8`;
     let handleError = () => {
       this.props.setWaiting(true);
       setTimeout(() => this.loadVideo(), 1000);
     }
 
-    fetch(dashUrl)
+    fetch(hlsUrl)
     .then((res) => {
-      if (res.status !== 200) {
+      if (res.status !== 200 && res.status !== 304) {
         return handleError();
       } else {
         this.props.setWaiting(false);
@@ -64,18 +64,19 @@ class ImmutableVideo extends React.Component {
           player.play();
         });
       } else if (this.hasMSE()) {
-        // Need to invoke dashjs player via the API, rather than via the
-        // automatic parsing of source on video elements, because otherwise we
-        // get weird behavior with playback freezing after the first segment.
-        let player = new shaka.Player(this.refs.plenaryVideo);
-        player.addEventListener("error",  (event) => {
-          console.log("ERROR!!!!!!!!!!!", error);
-          this.props.setError(error);
+        var hls = new Hls();
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+          console.log("PLAY!");
+          this.refs.plenaryVideo.play().catch((err) => {
+            console.log(err);
+            this.props.setError({error: "UNSUPPORTED"});
+          });
         });
-        player.load(dashUrl).then(function() {
-          console.log("The player has been loaded!");
+        hls.on(Hls.Events.ERROR, (event, data) => {
+          console.log("Hls.Events.ERROR", event, data);
         });
-        window.player = player
+        hls.attachMedia(this.refs.plenaryVideo);
+        hls.loadSource(hlsUrl);
       }
     })
     .catch((err) => {
@@ -86,13 +87,13 @@ class ImmutableVideo extends React.Component {
   render() {
     // Options are:
     // 1. Flash: use video.js + rtmp. Load src in componentDidMount.
-    // 2. MSE: use dash.js + MPEG-DASH. (Covers all but iOS). Load src in
-    //    componentDidMount. If the browser doesn't support h264 (eg. Chromium),
-    //    display an error message.
-    // 3. No MSE or flash: must be iOS. Pray for HLS via native video element.
+    // 2. MSE: hls.js. (Should cover all but iOS). Load src in
+    //    componentDidMount. If the browser doesn't support h264 (eg.
+    //    Chromium), display an error message.
+    // 3. No MSE or flash: must be iOS? Pray for HLS via native video element.
     let hlsUrl = `https://${this.props.conf.videoServerHostname}/hls/${this.getVideoPathName()}/index.m3u8`;
     return <div className='video-container' ref='videoContainer'>
-      <video autoPlay ref="plenaryVideo" id={`${this.props.conf.tag}-video`}>
+      <video controls ref="plenaryVideo" id={`${this.props.conf.tag}-video`}>
         { !this.hasFlash() && !this.hasMSE() ?
           <source src={hlsUrl} type='application/vnd.apple.mpegURL' />
         : ""}
@@ -108,7 +109,7 @@ export class VideoPlayer extends React.Component {
   }
 
   render() {
-    if (this.state.error && this.state.error.event === "MEDIA_ERR_SRC_NOT_SUPPORTED") {
+    if (this.state.error && this.state.error.error === "UNSUPPORTED") {
       return <div className='error'>
         Aw, snap. Your browser doesn't support any available video format.
       </div>
